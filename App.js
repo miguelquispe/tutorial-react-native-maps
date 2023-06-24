1f4acf55fca01a9ced2c98f8371745cc94689f9a
Miguel Quispe <dev@miguelquispe.com>
2023-06-23 22:54:12 -0500

    Tutorial Part 2

--- App.js
+++ App.js
@@ -1,15 +1,5 @@
-import { View } from "react-native";
 import MapScreen from "./screens/Map";
 
 export default function App() {
   return <MapScreen />;
 }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: "#fff",
-//     alignItems: "center",
-//     justifyContent: "center",
-//   },
-// });
--- screens/Map.js
+++ screens/Map.js
@@ -1,18 +1,42 @@
+import { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
-import MapView from "react-native-maps";
+import MapView, { Marker, PROVIDER_GOOGLE } from "react-native-maps";
 
 // Lima, PE
 const initialRegion = {
   latitude: -12.05798,
   longitude: -77.037132,
-  longitudeDelta: 0.04,
-  latitudeDelta: 0.02,
+  longitudeDelta: 0.03,
+  latitudeDelta: 0.025,
 };
 
 export default function MapScreen() {
+  const [currentRegion, setCurrentRegion] = useState(initialRegion);
+
+  const handleRegionChangeComplete = (region) => {
+    setCurrentRegion(region);
+  };
+
   return (
     <View style={styles.container}>
-      <MapView style={styles.map} initialRegion={initialRegion} />
+      <MapView
+        style={styles.map}
+        initialRegion={initialRegion}
+        onRegionChangeComplete={handleRegionChangeComplete}
+        provider={PROVIDER_GOOGLE}
+      >
+        <Marker
+          coordinate={currentRegion}
+          pinColor="purple"
+          title="Lima, Perú"
+          image={require("../assets/shop-store-icon-2.png")}
+        />
+      </MapView>
+      <View style={styles.info}>
+        <Text>Región actual:</Text>
+        <Text>Latitude: {currentRegion.latitude}</Text>
+        <Text>Longitude: {currentRegion.longitude}</Text>
+      </View>
     </View>
   );
 }
@@ -24,5 +48,13 @@ const styles = StyleSheet.create({
   },
   map: {
     flex: 1,
+  },
+  info: {
+    padding: 20,
+    backgroundColor: "white",
+    position: "absolute",
+    bottom: 20,
+    left: "5%",
+    width: "90%",
   },
 });
